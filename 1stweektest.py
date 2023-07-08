3b4853d839ff074c978c52d0c1ccf503b68ec929
Priya <kuntalpriya6@gmail.com>
2023-07-08 20:10:43 +0530

    video added

--- 1stweektest.py
+++ 1stweektest.py
@@ -1,8 +1,12 @@
 contact = {}
 
+def display_contact():
+    print("Name\t\tContact Number")
+    for key in contact:
+        print("{}\t\t{}".format(key,contact.get(key)))
 
 while True:
-    choice = str(input("1.Add the contact\n2.Search contact\n3.Updating contact information "))
+    choice = int(input("1.Add the contact\n2.Search contact\n3.Display contact\n4.Updating contact information\n5.Delete contact\nEnter your choice"))
     if choice == 1:
         name = input("Enter the contact name ")
         phone = input("Enter the phone number")
@@ -14,13 +18,28 @@ while True:
         else:
             print("Name is not found in contact book")
     elif choice == 3:
+       if not contact:
+           print("empty contact book")
+       else:
+           display_contact()
+    elif choice == 4:
         update_contact = input("Enter the contact to be updated")
         if update_contact in contact:
             phone = input("Enter the phone number")
             contact[update_contact]=phone
             print("contact updated")
+            display_contact()
         else:
-            print("Name is not found in contact book")
+            print("Name is not found in contact book") 
+    elif choice == 5:
+        del_contact = input("Enter the contact to be deleted ")
+        if del_contact in contact:
+            confirm = input("Do you want to delete this contact y/n")
+            if confirm == 'y' or confirm == 'Y':
+                contact.pop(del_contact)
+            display_contact()
+        else:
+            print("Name is not found in contact book")  
     else:
         break
 
